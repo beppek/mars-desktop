901673127486485fa00ebf3593eacf22874a01b6
Beppe Karlsson <beppe@live.se>
2016-01-02 21:35:55 +0100

    toggle menu

--- examination/client/source/js/app.js
+++ examination/client/source/js/app.js
@@ -12,21 +12,28 @@ var $ = require('jQuery');
 
 $(document).ready(function() {
 
-    $("#startMemory").click(function(event) {
+    $("#startMemory").click(function() {
 
         console.log("Memory time!");
 
     });
 
-    $("#startChat").click(function(event) {
+    $("#startChat").click(function() {
 
         console.log("Chat away!");
 
     });
 
-    $("#startMyApp").click(function(event) {
+    $("#startMyApp").click(function() {
 
         console.log("Nonono you don't get to see it yet!");
+
+    });
+
+    $("#menuToggle").click(function(event) {
+
+        event.preventDefault();
+        $(".menuList").toggle();
 
     });
 
