a0f97c3ebdc82ab84e138f9436463bc0acaddf2b
Beppe Karlsson <beppe@live.se>
2016-01-04 20:35:05 +0100

    Optimizes dragability and brings div to front on click

--- examination/client/source/js/Window.js
+++ examination/client/source/js/Window.js
@@ -11,6 +11,8 @@
 var $ = require('jQuery');
 
 var dragObject;
+var xPos;
+var yPos;
 
 /**
  *
@@ -19,6 +21,12 @@ var dragObject;
  * @param {string} name of app that opens in the window
  * */
 function Window(app) {
+
+    $(document).on('DOMNodeInserted', function(e) {
+        if (e.target.className === 'window') {
+            e.target.style.zIndex = 3;
+        }
+    });
 
     var template = document.querySelector("#windowTemplate");
     var tClone = document.importNode(template.content, true);
@@ -51,7 +59,15 @@ Window.prototype.addListeners = function() {
     $(".statusbar").mousedown(function(event) {
 
         dragObject = event.target.parentNode;
-        that.mouseDown();
+        that.mouseDown(event);
+
+    });
+
+    $(".window").mousedown(function() {
+
+        $(this).css("z-index", 2);
+        $(this).siblings(".window").css("z-index", 1);
+        console.log(this);
 
     });
 
@@ -68,9 +84,12 @@ Window.prototype.addListeners = function() {
  * Fires an eventlistener to listen for the mouse to move
  * @memberof Window.prototype
  * */
-Window.prototype.mouseDown = function() {
+Window.prototype.mouseDown = function(event) {
 
     var that = this;
+
+    xPos = event.clientX - dragObject.offsetLeft;
+    yPos = event.clientY - dragObject.offsetTop;
 
     window.addEventListener('mousemove', that.drag, true);
 
@@ -87,8 +106,8 @@ Window.prototype.drag = function(event) {
     event.stopPropagation();
 
     dragObject.style.position = "absolute";
-    dragObject.style.top = event.clientY + "px";
-    dragObject.style.left = event.clientX + "px";
+    dragObject.style.top = (event.clientY - yPos) + "px";
+    dragObject.style.left = (event.clientX - xPos) + "px";
 
 };
 
