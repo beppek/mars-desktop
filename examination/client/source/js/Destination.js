360c5f00fa7a9264d9dac2250d0503420e9b8400
Beppe Karlsson <beppe@live.se>
2016-01-11 22:57:54 +0100

    Game complete

--- examination/client/source/js/Destination.js
+++ examination/client/source/js/Destination.js
@@ -1,7 +1,7 @@
 /**
  *
  * @author: beppek
- * @version: 1.0.0
+ * @version: 0.1.0
  *
  * */
 
@@ -19,18 +19,32 @@ var questions = [
     require("./questions/7.json")
 ];
 
-
+/**
+ *
+ * This is the constructor function of a quiz game in the spirit of the old backpacker games of my childhood
+ * @contsructor
+ * @param {object} currentWin - the window to display the game in
+ * */
 function Destination(currentWin) {
 
+    //this.index = 0;
+    //localStorage.removeItem("savedgame");
+    //localStorage.removeItem("correctanswers");
+    //localStorage.setItem("savedgame", 6);
+    //localStorage.setItem("correctanswers", 6);
+
+    //Assign to the type for easo of access and change class of window to size up
     this.currentWin = currentWin;
     this.currentWin.classList.add("destinationWindow");
 
-    var winWorkSpace = currentWin.querySelectorAll(".winWorkSpace")[0];
+    this.winWorkSpace = currentWin.querySelectorAll(".winWorkSpace")[0];
 
-    var template = document.querySelectorAll("#myAppContent")[0].content;
+    //Find templates and start importing
+    //Assign to the type for ease of access
+    this.template = document.querySelectorAll("#myAppContent")[0].content;
 
-    this.boxWrapper = document.importNode(template.firstElementChild, true);
-    this.contentTemplate = template.querySelectorAll("template")[0].content;
+    this.boxWrapper = document.importNode(this.template.firstElementChild, true);
+    this.contentTemplate = this.template.querySelectorAll("template")[0].content;
 
     this.info = document.importNode(this.contentTemplate.firstElementChild, true);
     this.infoPTag = this.info.querySelectorAll("p")[0];
@@ -39,26 +53,61 @@ function Destination(currentWin) {
     this.answerOptionsDiv = this.questionDiv.querySelectorAll(".answerOptions")[0];
     this.answerOption = this.answerOptionsDiv.querySelectorAll("template")[0].content.firstElementChild;
 
-    winWorkSpace.appendChild(this.boxWrapper);
+    this.winWorkSpace.appendChild(this.boxWrapper);
 
+    //Check if menu already present before adding
+    if (!currentWin.querySelectorAll(".winMenu")[0]) {
+
+        //Insert window menu and populate
+        var menuTemplate = currentWin.querySelector("#winMenu");
+        var menuDiv = document.importNode(menuTemplate.content.firstElementChild, true);
+
+        var menuItem = menuDiv.querySelectorAll(".winMenuItem")[0];
+
+        menuItem.textContent = "Save Game";
+
+        menuItem.addEventListener("click", function (event) {
+
+            event.preventDefault();
+            this.saveGame(this.index);
+            return;
+
+        }.bind(this));
+
+        this.winWorkSpace.parentNode.insertBefore(menuDiv, this.winWorkSpace);
+
+    }
+
+    //Check if save game exists before start up
     if (localStorage.savedgame) {
 
-        var question = localStorage.savedGame;
+        var question = localStorage.savedgame;
+        this.correctAnswers = parseInt(localStorage.correctanswers);
+        console.log(this.correctAnswers);
 
         this.printInfo(question);
 
     }else {
 
         this.printInfo(0);
+        this.correctAnswers = 0;
 
     }
 
 }
 
+/**
+ *
+ * This function prints the info from a json object
+ * @memberof Destination.prototype
+ * @param {int} index - the question to print
+ * */
 Destination.prototype.printInfo = function(index) {
 
+    //Add class to display the right background image
     this.currentWin.classList.add("question" + index);
 
+    //Populate UI and add event listener
     this.infoPTag.appendChild(document.createTextNode(questions[index].info));
 
     var next = this.info.querySelectorAll(".continue")[0].firstElementChild;
@@ -66,8 +115,11 @@ Destination.prototype.printInfo = function(index) {
     $(next).click(function(event) {
 
         event.preventDefault();
+
+        //remove event listener to avoid doubles
         $(next).unbind();
 
+        //clear content divs
         this.clearDiv(this.boxWrapper);
         this.clearDiv(this.infoPTag);
 
@@ -79,24 +131,38 @@ Destination.prototype.printInfo = function(index) {
 
 };
 
-Destination.prototype.clearDiv = function(div) {
+/**
+ *
+ * This function clears the element passed of any content
+ * @memberof Destination.prototype
+ * @param {object} element - the element to be cleared of content
+ * */
+Destination.prototype.clearDiv = function(element) {
 
     //Remove content from div
-    while (div.firstChild) {
+    while (element.firstChild) {
 
-        div.removeChild(div.firstChild);
+        element.removeChild(element.firstChild);
 
     }
 
 };
 
+/**
+ *
+ * This function prints the question to the UI
+ * @memberof Destination.prototype
+ * @param {int} index - the question to be displayed
+ * */
 Destination.prototype.printQuestion = function(index) {
 
+    //Populate UI
     var questionText = this.questionDiv.firstElementChild;
     questionText.textContent = questions[index].question;
 
     this.boxWrapper.appendChild(this.questionDiv);
 
+    //Loop through the options, add to UI and add event listener
     questions[index].options.forEach(function(option) {
 
         var optionTag = document.importNode(this.answerOption, true);
@@ -120,10 +186,17 @@ Destination.prototype.printQuestion = function(index) {
 
 };
 
+/**
+ *
+ * This function checks the answer against the json object
+ * @memberof Destination.prototype
+ * @param {string} answer - a string containing the answer from the user
+ * @param {int} index - the question that was answered
+ * */
 Destination.prototype.checkAnswer = function(answer, index) {
 
+    //Add event listener
     var next = this.info.querySelectorAll(".continue")[0].firstElementChild;
-
     $(next).click(function(event) {
 
         event.preventDefault();
@@ -132,18 +205,31 @@ Destination.prototype.checkAnswer = function(answer, index) {
         this.clearDiv(this.boxWrapper);
         this.clearDiv(this.infoPTag);
 
+        //Remove class so next image can be displayed
         this.currentWin.classList.remove("question" + index);
 
         index += 1;
+        this.index = index;
 
-        this.printInfo(index);
+        if (this.index < questions.length) {
+
+            this.printInfo(index);
+
+        }else {
+
+            this.finishedGame();
+        }
+
 
     }.bind(this));
 
+    //Populate UI
     this.boxWrapper.appendChild(this.info);
 
+    //Check the answers against the json object
     if (answer === questions[index].rightAnswer) {
 
+        this.correctAnswers += 1;
         this.infoPTag.appendChild(document.createTextNode(questions[index].correct));
         this.boxWrapper.appendChild(this.info);
 
@@ -156,9 +242,50 @@ Destination.prototype.checkAnswer = function(answer, index) {
 
 };
 
+/**
+ *
+ * This function displays the result after the game is finished
+ * @memberof Destination.prototype
+ * */
+Destination.prototype.finishedGame = function() {
+
+    this.currentWin.classList.remove("destinationWindow");
+    this.clearDiv(this.winWorkSpace);
+
+    var template = this.template.querySelectorAll("template")[1].content;
+
+    var resultsDiv = document.importNode(template.firstElementChild, true);
+
+    resultsDiv.querySelectorAll("p")[0].appendChild(document.createTextNode(this.correctAnswers + " correct answers!"));
+
+    var playAgain = resultsDiv.querySelectorAll("a")[0];
+
+    $(playAgain).click(function(event) {
+
+        event.preventDefault();
+        $(playAgain).unbind();
+        this.clearDiv(this.winWorkSpace);
+        new Destination(this.currentWin);
+
+    }.bind(this));
+
+    this.winWorkSpace.appendChild(resultsDiv);
+
+    localStorage.removeItem("savedgame");
+    localStorage.removeItem("correctanswers");
+
+};
+
+/**
+ *
+ * This function saves the game in the local storage of the browser
+ * @memberof Destination.prototype
+ * @param {int} index - the question to save the game at
+ * */
 Destination.prototype.saveGame = function(index) {
 
     localStorage.setItem("savedgame", index);
+    localStorage.setItem("correctanswers", this.correctAnswers);
 
 };
 
